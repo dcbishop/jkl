42a7f1bc335258437ce72c078714abea47431dea
David C. Bishop <david@davidbishop.org>
2014-09-24 18:02:30 +1000

    [Refactor]: Extract some functions.

--- cli/cli.go
+++ cli/cli.go
@@ -26,31 +26,47 @@ type Options struct {
 
 // ParseArgs takes arguments and returns a cli.Options. Will return error if parsing failed.
 func ParseArgs(args []string) (Options, error) {
-	options := Options{}
-
 	if len(args) < 2 {
-		return options, nil
+		return Options{}, nil
 	}
 
+	options, err := parseWithDocopt(args[1:])
+	return options, err
+}
+
+// parseWithDocopt takes a slice of args (without the program name) and returns an Options
+func parseWithDocopt(args []string) (Options, error) {
 	// Docopt.go doesn't seem to have a way to stop it spamming the console.
 	disableStdout()
 	defer restoreStdout()
 
-	version := globals.Name() + " " + globals.VersionString()
-
-	arguments, err := docopt.Parse(Usage(), args[1:], false, version, false, false)
+	version := nameVersion()
+	arguments, err := docopt.Parse(Usage(), args, false, version, false, false)
 	if err != nil {
-		return options, err
+		return Options{}, err
 	}
 
+	options := docoptArgsToOptions(arguments)
+
+	return options, nil
+}
+
+// Returns the name and version (ie "Gim 0.1")
+func nameVersion() string {
+	return globals.Name() + " " + globals.VersionString()
+}
+
+// Converts the result of docopt.Parse into an Options
+func docoptArgsToOptions(arguments map[string]interface{}) Options {
+	options := Options{}
 	if arguments["--help"].(bool) {
 		options.Help = true
-		return options, nil
+		return options
 	}
 
 	options.FilesToOpen = arguments["<file>"].([]string)
 
-	return options, nil
+	return options
 }
 
 // Usage returns the usage message
@@ -60,11 +76,13 @@ func Usage() string {
 
 var initialStdout = os.Stdout
 
+// Replaces the stdout with a dummy pipe to stop console output.
 func disableStdout() {
 	_, w, _ := os.Pipe()
 	os.Stdout = w
 }
 
+// Restores the stdout to the writer it has at program start.
 func restoreStdout() {
 	os.Stdout = initialStdout
 }
