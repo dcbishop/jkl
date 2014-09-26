7d5e353a5ff15029edc0183edc339bcb3a8682ec
David C. Bishop <david@davidbishop.org>
2014-09-26 16:58:50 +1000

    Add Running() to query runstate. Stop() will block until finished.

--- ui/ui.go
+++ ui/ui.go
@@ -1,23 +1,80 @@
 package ui
 
 import (
+	"errors"
 	"log"
+	"sync"
+	"time"
 
 	"github.com/nsf/termbox-go"
 )
 
+// Service implementations have a Run() loop that does stuff and blocks untill Stop() is called
+type Service interface {
+	Run()
+	Stop()
+	Running() bool
+}
+
+// WaitUntilServiceRunning will wait until service.Running() returns state,
+// will return an error if it took longer than timeout.
+func WaitUntilServiceRunning(service Service, state bool, timeout time.Duration) error {
+	start := time.Now()
+
+	for !service.Running() == state {
+		time.Sleep(time.Millisecond)
+		if time.Since(start) >= timeout {
+			return errors.New("Time out")
+		}
+	}
+
+	return nil
+}
+
+// UI handles input and displaying the app
+type UI interface {
+	Service
+}
+
 // TermboxUI handles ui using termbox-go
 type TermboxUI struct {
-	quit chan bool
+	quit         chan bool
+	running      bool
+	runningMutex sync.Mutex
 }
 
 // Run enters the main UI loop untill Stop() is called.
 func (tbw *TermboxUI) Run() {
+	if tbw.setRunning(true) {
+		panic("UI already running.")
+		return
+	}
+	defer tbw.setRunning(false)
+
 	tbw.initialize()
 
 	go tbw.handleEvents()
 	tbw.waitForQuit()
 	tbw.cleanUp()
+}
+
+func (tbw *TermboxUI) setRunning(state bool) (wasAlreadySet bool) {
+	tbw.runningMutex.Lock()
+	defer tbw.runningMutex.Unlock()
+
+	if tbw.running == state {
+		return true
+	}
+
+	tbw.running = state
+	return false
+}
+
+// Running returns true if ui.Run() was called but ui.Stop() hasn't been.
+func (tbw *TermboxUI) Running() bool {
+	tbw.runningMutex.Lock()
+	defer tbw.runningMutex.Unlock()
+	return tbw.running
 }
 
 // Stop terminates the Run loop.
@@ -26,6 +83,7 @@ func (tbw *TermboxUI) Stop() {
 		return
 	}
 	close(tbw.quit)
+	WaitUntilServiceRunning(tbw, false, time.Second)
 }
 
 func (tbw *TermboxUI) initialize() {
--- ui/ui_test.go
+++ ui/ui_test.go
@@ -7,29 +7,66 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestRunStop(t *testing.T) {
-	Convey("ui.Run() should be terminated by an ui.Stop()", t, func() {
+func TestWaitUntilServiceRunning(t *testing.T) {
+	Convey("Timeout should return error", t, func() {
 		ui := TermboxUI{}
-		time.AfterFunc(time.Second/5, func() {
+		err := WaitUntilServiceRunning(&ui, true, 1)
+		So(err, ShouldNotBeNil)
+	})
+	Convey("Should proceed when running state is met", t, func() {
+		ui := TermboxUI{}
+		err := WaitUntilServiceRunning(&ui, false, 1)
+		So(err, ShouldBeNil)
+	})
+}
+
+func TestRunStop(t *testing.T) {
+	Convey("ui.Run() should be terminated by ui.Stop() and ui.Running() should give the correct status", t, func() {
+		ui := TermboxUI{}
+
+		go ui.Run()
+
+		if WaitUntilServiceRunning(&ui, true, time.Second) != nil {
+			panic("Failed to start UI.")
+		}
+		So(ui.Running(), ShouldBeTrue)
+
+		ui.Stop()
+
+		if WaitUntilServiceRunning(&ui, false, time.Second) != nil {
 			panic("Failed to stop UI.")
-		})
+		}
+		So(ui.Running(), ShouldBeFalse)
+
+	})
+	Convey("ui.Stop() at the same time as ui.Run()", t, func() {
+		ui := TermboxUI{}
 		go ui.Stop()
 		ui.Run()
-	})
-	Convey("ui.Run() should be terminated by an ui.Stop() when blocking", t, func() {
-		ui := TermboxUI{}
-		time.AfterFunc(time.Second/10, func() {
-			ui.Stop()
-		})
-		time.AfterFunc(time.Second/5, func() {
-			panic("Failed to stop UI.")
-		})
-		ui.Run()
+		So(ui.Running(), ShouldBeFalse)
 	})
 	Convey("ui.Stop() without having called ui.Run() shouldn't explode", t, func() {
 		ui := TermboxUI{}
 		ui.Stop()
 		ui.Stop()
 		ui.Stop()
+		So(ui.Running(), ShouldBeFalse)
+	})
+	Convey("ui.Run() when already running should panic", t, func() {
+		ui := TermboxUI{}
+		go ui.Run()
+
+		if WaitUntilServiceRunning(&ui, true, time.Second) != nil {
+			panic("Failed to start UI.")
+		}
+
+		So(ui.Running(), ShouldBeTrue)
+		So(ui.Run, ShouldPanic)
+		So(ui.Running(), ShouldBeTrue)
+		ui.Stop()
+
+		if WaitUntilServiceRunning(&ui, false, time.Second) != nil {
+			panic("Failed to start UI.")
+		}
 	})
 }
