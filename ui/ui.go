ee4a864ba3feade05d1f9804f3c8bbcf722c79ca
David C. Bishop <david@davidbishop.org>
2014-09-27 10:28:40 +1000

    Move termbox UI components into their own file.

--- ui/ui.go
+++ ui/ui.go
@@ -1,11 +1,6 @@
 package ui
 
-import (
-	"time"
-
-	"github.com/dcbishop/gim/service"
-	"github.com/nsf/termbox-go"
-)
+import "github.com/dcbishop/gim/service"
 
 // UI handles input and displaying the app.
 type UI interface {
@@ -21,91 +16,4 @@ type Input interface {
 // Event holds information about an event
 type Event struct {
 	Data interface{}
-}
-
-// TermboxUI handles ui using termbox-go.
-type TermboxUI struct {
-	quit   chan bool
-	events chan Event
-	state  service.State
-}
-
-// Run enters the main UI loop untill Stop() is called.
-func (tbw *TermboxUI) Run() {
-	if tbw.state.SetRunning() != nil {
-		panic("UI already running.")
-		return
-	}
-	defer tbw.state.SetStopped()
-
-	tbw.initialize()
-
-	go tbw.handleEvents()
-	tbw.waitForQuit()
-	tbw.cleanUp()
-}
-
-// Running returns true if ui.Run() was called but ui.Stop() hasn't been.
-func (tbw *TermboxUI) Running() bool {
-	return tbw.state.Running()
-}
-
-// Stop terminates the Run loop.
-func (tbw *TermboxUI) Stop() {
-	if tbw.quit == nil {
-		return
-	}
-	close(tbw.quit)
-	service.WaitUntilRunning(tbw, time.Second)
-}
-
-// Events gets the channel that emits events
-func (tbw *TermboxUI) Events() <-chan Event {
-	return tbw.events
-}
-
-func (tbw *TermboxUI) initialize() {
-	tbw.initializeQuitChannel()
-	tbw.initializeEventChannel()
-	termbox.Init()
-}
-
-func (tbw *TermboxUI) cleanUp() {
-	termbox.Close()
-}
-
-func (tbw *TermboxUI) initializeQuitChannel() {
-	tbw.quit = make(chan bool)
-}
-
-func (tbw *TermboxUI) initializeEventChannel() {
-	tbw.events = make(chan Event)
-}
-
-func (tbw *TermboxUI) handleEvents() {
-loop:
-	for {
-		select {
-		case <-tbw.quit:
-			break loop
-		default:
-			tbw.handleEvent()
-		}
-	}
-}
-
-func (tbw *TermboxUI) handleEvent() {
-	event := termbox.PollEvent()
-	tbw.events <- termboxEventToInternal(event)
-}
-
-func termboxEventToInternal(event termbox.Event) Event {
-	return Event{event}
-}
-
-func (tbw *TermboxUI) waitForQuit() {
-	select {
-	case <-tbw.quit:
-		return
-	}
 }
