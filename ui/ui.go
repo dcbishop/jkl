16c8af3aab76fe0cb35fc62901e6d0e32432f49d
David C. Bishop <david@davidbishop.org>
2014-09-27 13:38:06 +1000

    Add FakeUI.

--- ui/ui.go
+++ ui/ui.go
@@ -1,6 +1,10 @@
 package ui
 
-import "github.com/dcbishop/jkl/service"
+import (
+	"time"
+
+	"github.com/dcbishop/jkl/service"
+)
 
 // UI handles input and displaying the app.
 type UI interface {
@@ -13,7 +17,57 @@ type Input interface {
 	Events() <-chan Event
 }
 
-// Event holds information about an event
+// Event holds information about an event.
 type Event struct {
 	Data interface{}
+}
+
+// FakeUI for disabling output, injecting input and testing.
+type FakeUI struct {
+	state     service.State
+	EventChan chan Event
+	Quit      chan bool
+}
+
+// Run starts the service. Will block until Stop() is called.
+func (ui *FakeUI) Run() {
+	if ui.state.SetRunning() != nil {
+		panic("Fake UI already running.")
+	}
+
+	ui.EventChan = make(chan Event)
+	ui.Quit = make(chan bool)
+
+	ui.loopUntilQuit()
+
+	ui.state.SetStopped()
+}
+
+// Stop the service
+func (ui *FakeUI) Stop() {
+	if !ui.Running() {
+		return
+	}
+	close(ui.Quit)
+	service.WaitUntilStopped(ui, time.Second)
+}
+
+// Running returns the running state of the service
+func (ui *FakeUI) Running() bool {
+	return ui.state.Running()
+}
+
+// Events returns the channel that produces events
+func (ui *FakeUI) Events() <-chan Event {
+	return ui.EventChan
+}
+
+func (ui *FakeUI) loopUntilQuit() {
+loop:
+	for {
+		select {
+		case <-ui.Quit:
+			break loop
+		}
+	}
 }
--- ui/ui_test.go
+++ ui/ui_test.go
@@ -8,53 +8,61 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestRunStop(t *testing.T) {
-	Convey("ui.Run() should be terminated by ui.Stop() and ui.Running() should give the correct status", t, func() {
+func TestFakeUI(t *testing.T) {
+	Convey("A new FakeUI should be in a stopped state", t, func() {
+		ui := FakeUI{}
+		testUI(&ui)
+	})
+}
+
+func TestTermbox(t *testing.T) {
+	Convey("A new TermboxUI should be in a stopped state", t, func() {
 		ui := TermboxUI{}
+		testUI(&ui)
+	})
+}
 
+func testUI(ui UI) {
+	So(ui.Running(), ShouldBeFalse)
+	Convey("Run() should set state to Running() in a reasonable time", func() {
 		go ui.Run()
-
-		if service.WaitUntilRunning(&ui, time.Second) != nil {
+		if service.WaitUntilRunning(ui, time.Second) != nil {
 			panic("Failed to start UI.")
 		}
 		So(ui.Running(), ShouldBeTrue)
+		Convey("should be terminated by Stop() and Running() should give the correct status", func() {
+			ui.Stop()
 
-		ui.Stop()
+			if service.WaitUntilStopped(ui, time.Second) != nil {
+				panic("Failed to stop UI.")
+			}
+			So(ui.Running(), ShouldBeFalse)
 
-		if service.WaitUntilStopped(&ui, time.Second) != nil {
-			panic("Failed to stop UI.")
-		}
-		So(ui.Running(), ShouldBeFalse)
+		})
+		Convey("Run() when already running should panic", func() {
+			So(ui.Running(), ShouldBeTrue)
+			So(ui.Run, ShouldPanic)
+			So(ui.Running(), ShouldBeTrue)
+			ui.Stop()
 
+			if service.WaitUntilStopped(ui, time.Second) != nil {
+				panic("Failed to start UI.")
+			}
+		})
+		Convey("Event() should now have a valid channel", func() {
+			events := ui.Events()
+			So(events, ShouldNotBeNil)
+		})
 	})
-	Convey("ui.Stop() at the same time as ui.Run()", t, func() {
-		ui := TermboxUI{}
+	Convey("Stop() at the same time as ui.Run()", func() {
 		go ui.Stop()
 		ui.Run()
 		So(ui.Running(), ShouldBeFalse)
 	})
-	Convey("ui.Stop() without having called ui.Run() shouldn't explode", t, func() {
-		ui := TermboxUI{}
+	Convey("Stop() without having called Run() shouldn't explode", func() {
 		ui.Stop()
 		ui.Stop()
 		ui.Stop()
 		So(ui.Running(), ShouldBeFalse)
-	})
-	Convey("ui.Run() when already running should panic", t, func() {
-		ui := TermboxUI{}
-		go ui.Run()
-
-		if service.WaitUntilRunning(&ui, time.Second) != nil {
-			panic("Failed to start UI.")
-		}
-
-		So(ui.Running(), ShouldBeTrue)
-		So(ui.Run, ShouldPanic)
-		So(ui.Running(), ShouldBeTrue)
-		ui.Stop()
-
-		if service.WaitUntilStopped(&ui, time.Second) != nil {
-			panic("Failed to start UI.")
-		}
 	})
 }
