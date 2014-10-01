722a652548515d199dc7ba92c2b3f7f367b260b7
David C. Bishop <david@davidbishop.org>
2014-10-01 12:40:40 +1000

    Add Cursor tests.

--- editor/editor_test.go
+++ editor/editor_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/dcbishop/fileaccessor"
+	"github.com/dcbishop/jkl/buffer"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -37,6 +38,67 @@ func TestOpenFile(t *testing.T) {
 			So(len(editor.Buffers()), ShouldEqual, 1)
 			So(editor.buffers[0].Data(), ShouldResemble, []byte{})
 			So(editor.buffers[0].Filename(), ShouldResemble, "file2.txt")
+		})
+	})
+}
+
+var data = []byte(`Hello, World!
+Line 2
+Line 3`)
+
+func TestCursor(t *testing.T) {
+	Convey("New cursor", t, func() {
+		buffer := buffer.New()
+		buffer.SetData(data)
+		cursor := Cursor{buffer: &buffer}
+
+		x, line := cursor.Position()
+
+		So(x, ShouldEqual, 0)
+		So(line, ShouldEqual, 0)
+		So(cursor.buffer, ShouldEqual, &buffer)
+
+		Convey("DownLine", func() {
+			x, line := cursor.DownLine()
+			So(x, ShouldEqual, 0)
+			So(line, ShouldEqual, 1)
+
+			Convey("Move DownLine", func() {
+				cursor.Move(cursor.DownLine())
+				x, line := cursor.Position()
+				So(x, ShouldEqual, 0)
+				So(line, ShouldEqual, 1)
+
+				Convey("UpLine", func() {
+					x, line := cursor.UpLine()
+					So(x, ShouldEqual, 0)
+					So(line, ShouldEqual, 0)
+				})
+			})
+
+		})
+
+		Convey("UpLine when at top", func() {
+			x, line := cursor.UpLine()
+			So(x, ShouldEqual, 0)
+			So(line, ShouldEqual, 0)
+		})
+
+		Convey("ForwardCharacter should move forward one character", func() {
+			x, _ := cursor.ForwardCharacter()
+			So(x, ShouldEqual, 1)
+
+			Convey("BackCharacter should move backward one character", func() {
+				cursor.Move(cursor.ForwardCharacter())
+				So(x, ShouldEqual, 1)
+				x, _ := cursor.BackCharacter()
+				So(x, ShouldEqual, 0)
+			})
+		})
+
+		Convey("BackCharacter when at first character shouldn't move", func() {
+			x, _ := cursor.BackCharacter()
+			So(x, ShouldEqual, 0)
 		})
 	})
 }
